9c77b926166ba0919d854e56c6737e692525f3b1
callum li <callumli@callums-MacBook-Pro.local>
2017-11-21 17:02:56 +1300

    Fixed up tests, check style

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -2,52 +2,47 @@
 require_relative '../model'
 
 describe Model do
-  #let(:model) { Model.new(starting_number_of_lives: 6, word_to_guess: "alphabet") }
+  let(:model) { Model.new(starting_number_of_lives: 6, word_to_guess: "alphabet") }
 
   context "in a game with 6 lives, and \"alphabet\" as the hidden word" do
-    subject { Model.new(starting_number_of_lives: 6, word_to_guess: "alphabet") }
-
     it "should have the word to guess as \"alphabet\"" do
-      expect(subject.word_to_guess).to eq("alphabet")
+      expect(model.word_to_guess).to eq("alphabet")
     end
 
     context "initially" do
-      #subject { model }
       it "should have six lives remaining" do
-        expect(subject.remaining_lives).to eq(6)
+        expect(model.remaining_lives).to eq(6)
       end
 
       it "should have no letters guessed" do
-        expect(subject.guessed_letters).to be_empty
-        expect(subject.letter_has_been_guessed?("a")).to be false
-        expect(subject.letter_has_been_guessed?("g")).to be false
-        expect(subject.letter_has_been_guessed?("z")).to be false
+        expect(model.guessed_letters).to be_empty
+        expect(model.letter_has_been_guessed?("a")).to be false
+        expect(model.letter_has_been_guessed?("g")).to be false
+        expect(model.letter_has_been_guessed?("z")).to be false
       end
 
       it "should not be game over" do
-        expect(subject.game_over?).to be false
+        expect(model.game_over?).to be false
       end
 
       it "should not be won" do
-        expect(subject.game_won?).to be false
+        expect(model.game_won?).to be false
       end
     end
 
     context "after guessing \"a\"" do
-      before(:all) { subject.apply_guess("a") }
+      before { model.apply_guess("a") }
 
       it "should have six lives remaining" do
-        subject.apply_guess("a")
-        expect(subject.remaining_lives).to eq(6)
+        model.apply_guess("a")
+        expect(model.remaining_lives).to eq(6)
       end
 
       it "should have only \"a\" guessed" do
-        before{subject.apply_guess("a")}
-        expect(subject.guessed_letters.length).to eq(1)
-        # it {is_expected.to be true}
-        expect(subject.letter_has_been_guessed?("a")).to be true
-        expect(subject.letter_has_been_guessed?("g")).to be false
-        expect(subject.letter_has_been_guessed?("z")).to be false
+        expect(model.guessed_letters.length).to eq(1)
+        expect(model.letter_has_been_guessed?("a")).to be true
+        expect(model.letter_has_been_guessed?("g")).to be false
+        expect(model.letter_has_been_guessed?("z")).to be false
       end
     end
 
